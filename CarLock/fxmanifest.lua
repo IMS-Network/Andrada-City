1ea45b8d62a427ee4a01225631a66eddd8f9e255
thefourCraft <COO@ims-network.net>
2022-12-06 17:29:58 +0200

    Create fxmanifest.lua

---
+++ CarLock/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'Your Name'
+description 'A vehicle locking and unlocking system for GTA V'
+version '1.0.0'
+
+client_scripts {
+  'vehiclelocking.lua'
+}
+
+server_scripts {
+  'vehiclelocking.lua'
+}
