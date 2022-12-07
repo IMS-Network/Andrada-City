74d5a21e9a06a86f8c5594e0e1d38719943bd122
IMS - Network | Editor <107467857+IMS-Network-Editor@users.noreply.github.com>
2022-12-07 13:12:52 +0200

    Update radio.lua

--- Radio/client/radio.lua
+++ Radio/client/radio.lua
@@ -1,19 +1,70 @@
 -- client/radio.lua
 
--- Define the custom radio stations
-local stations = {
-  {name = "My Radio Station 1", url = "https://example.com/station1.mp3"},
-  {name = "My Radio Station 2", url = "https://example.com/station2.mp3"},
-  {name = "My Radio Station 3", url = "https://example.com/station3.mp3"},
-  ...
+-- Define the ESX framework
+ESX = nil
+
+-- Wait for the ESX framework to be loaded
+Citizen.CreateThread(function()
+  while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(0)
+  end
+end)
+
+-- Define the custom radio station
+local station = {
+  name = "My Radio Station",
+  url = "https://cdn.cybercdn.live/Pervoia/Audio/icecast.audio"
 }
+
+-- Load the radio station configuration from the radio-settings.yml file
+Citizen.CreateThread(function()
+  local cfg = LoadResourceFile(GetCurrentResourceName(), "radio-settings.yml")
+  if cfg then
+    station = yaml.decode(cfg)
+  end
+end)
+
+-- Register a custom menu type for the radio station menu
+ESX.UI.Menu.RegisterType("radio_station", function(data)
+  local elements = {}
+
+  -- Add a label to the menu
+  table.insert(elements, {
+    label = "Press ~INPUT_PICKUP~ to change the radio station"
+  })
+
+  return {
+    type = "menu",
+    name = "radio_station",
+    title = "Radio Station",
+    elements = elements
+  }
+end)
 
 -- Set the audio flag to allow custom URLs to be played as radio stations
 SetAudioFlag("AllowCustomUrl", true)
 
--- Add the custom radio stations to the game
+-- Add the custom radio station to the game
 Citizen.CreateThread(function()
-  for i,station in ipairs(stations) do
-    AddRadioStation(station.name, station.url)
-  end
+  AddRadioStation(station.name, station.url)
 end)
+
+-- Main loop
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+
+    -- Check if the player is in a vehicle
+    if IsPedInAnyVehicle(PlayerPedId(), false) then
+      -- Check if the player pressed the input to open the radio station menu
+      if IsControlJustPressed(0, 38) then
+        -- Open the radio station menu
+        ESX.UI.Menu.Open("radio_station", {}, function(data, menu)
+          -- Check if the player pressed the input to change the radio station
+          if IsControlJustPressed(0, 38) then
+            -- Change the radio station
+            SetRadioToStationName(station.name)
+
+            -- Notify the player that the radio station was changed
+            ESX.ShowNotification("Changed radio station
