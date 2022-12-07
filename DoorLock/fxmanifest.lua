5913b0dd8634081197b9a09431730d5a1302c842
IMS - Network | Editor <107467857+IMS-Network-Editor@users.noreply.github.com>
2022-12-07 11:48:40 +0200

    Update fxmanifest.lua

--- DoorLock/fxmanifest.lua
+++ DoorLock/fxmanifest.lua
@@ -2,13 +2,22 @@ fx_version 'adamant'
 
 game 'gta5'
 
+description 'Door lock script for FiveM using the ESX framework'
+
+version '1.0.0'
+
 server_scripts {
   'server/main.lua',
-  'server/doorlock.lua',
   '@mysql-async/lib/MySQL.lua',
+  '@es_extended/locale.lua'
 }
 
 client_scripts {
-  'client/main.lua',
   'client/doorlock.lua',
+  '@es_extended/locale.lua'
+}
+
+dependencies {
+  'es_extended',
+  'mysql-async'
 }
